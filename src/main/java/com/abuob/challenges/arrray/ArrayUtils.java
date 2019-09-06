5b116fa88ec819aa7f2e01e1beb38c7ece45f172
Alex Buob <alexander.buob@gmail.com>
2019-09-05 23:16:15 -0500

    added getEquilibriumIndices() to ArrayUtils.java

--- src/main/java/com/abuob/challenges/arrray/ArrayUtils.java
+++ src/main/java/com/abuob/challenges/arrray/ArrayUtils.java
@@ -1,7 +1,6 @@
 package com.abuob.challenges.arrray;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public final class ArrayUtils {
 
@@ -31,5 +30,33 @@ public final class ArrayUtils {
             }
         }
         return answer;
+    }
+
+    /*
+      A zero-indexed array A consisting of N integers is given.
+      An equilibrium index of this array is any integer P such that 0 ≤ P < N
+      and the sum of elements of lower indices is equal to the sum of elements of higher indices, i.e.
+
+      A[0] + A[1] + ... + A[P−1] = A[P+1] + ... + A[N−2] + A[N−1].
+
+      Sum of zero elements is assumed to be equal to 0. This can happen if P = 0 or if P = N−1.
+    */
+    public static List<Integer> getEquilibriumIndices(int[] array) {
+        List<Integer> answerList = new ArrayList<>();
+
+        long totalSum = sum(array);
+        long lowSum = 0L;
+        for (int i = 0; i < array.length; i++) {
+            totalSum -= array[i];
+            if (lowSum == totalSum) {
+                answerList.add(i);
+            }
+            lowSum += array[i];
+        }
+        return answerList;
+    }
+
+    private static long sum(int[] array) {
+        return Arrays.stream(array).mapToLong(Long::valueOf).sum();
     }
 }
--- src/test/java/com/abuob/challenges/array/ArrayUtilsTest.java
+++ src/test/java/com/abuob/challenges/array/ArrayUtilsTest.java
@@ -1,6 +1,7 @@
 package com.abuob.challenges.array;
 
 import com.abuob.challenges.arrray.ArrayUtils;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,5 +13,11 @@ public class ArrayUtilsTest {
         assertThat(ArrayUtils.smallestPositiveInteger(new int[]{1, 3, 6, 4, 1, 2})).isEqualTo(5);
         assertThat(ArrayUtils.smallestPositiveInteger(new int[]{1, 2, 3})).isEqualTo(4);
         assertThat(ArrayUtils.smallestPositiveInteger(new int[]{-1, -3})).isEqualTo(1);
+    }
+
+    @Test
+    public void getEquilibriumIndices() {
+        assertThat(ArrayUtils.getEquilibriumIndices(new int[]{-1, 3, -4, 5, 1, -6, 2, 1}))
+                .isEqualTo(Lists.newArrayList(1,3,7));
     }
 }
