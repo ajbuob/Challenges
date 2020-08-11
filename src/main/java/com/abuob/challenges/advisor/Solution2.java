1cc20686eeab23cbb99b96b61434070d1f25a528
Alex Buob <alexander.buob@gmail.com>
2020-08-11 14:27:03 -0500

    Added cache contents

---
+++ src/main/java/com/abuob/challenges/advisor/Solution2.java
@@ -0,0 +1,70 @@
+package com.abuob.challenges.advisor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Solution2 {
+
+    public static List<Integer> cacheContents(List<List<Integer>> callLogs) {
+
+        List<Integer> defaultList = new ArrayList<>();
+        defaultList.add(-1);
+
+        Map<Integer, List<Integer>> timeToItemsMap = new TreeMap<>();
+
+        Map<Integer, Integer> itemToPriorityMap = new TreeMap<>();
+
+        //Populate each time with all items updated
+        callLogs.forEach(callLog -> {
+            List<Integer> listOfItems =
+                    timeToItemsMap.getOrDefault(callLog.get(0), new ArrayList<>());
+            listOfItems.add(callLog.get(1));
+            timeToItemsMap.put(callLog.get(0), listOfItems);
+        });
+
+        Integer currentTime;
+        Map<Integer, Long> itemsFrequencyMap;
+
+        List<Integer> currentItems;
+
+        //Process timeToPrioritiesMap -> itemToPriorityMap
+        for (Map.Entry<Integer, List<Integer>> entry : timeToItemsMap.entrySet()) {
+            currentTime = entry.getKey();
+            currentItems = entry.getValue();
+
+            //Adjust priority map to take in account the current passage of time
+            itemToPriorityMap = itemToPriorityMap.entrySet().stream()
+                    .collect(Collectors.toMap(Map.Entry::getKey,
+                            e -> e.getValue() > 0 ? e.getValue() - 1 : e.getValue()));
+
+            //for each time, compute the priority
+            itemsFrequencyMap = currentItems.stream()
+                    .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+            Integer currentItem;
+            Integer currentPriority, newPriority;
+            Long frequency;
+
+            //compute the frequency/newPriority of each item at the current time
+            for (Map.Entry<Integer, Long> frequencyEntry : itemsFrequencyMap.entrySet()) {
+                currentItem = frequencyEntry.getKey();
+                frequency = frequencyEntry.getValue();
+
+                currentPriority = itemToPriorityMap.getOrDefault(currentItem, 0);
+                newPriority = Math.toIntExact(currentPriority + frequency * currentTime);
+
+                itemToPriorityMap.put(currentItem, newPriority);
+            }
+        }
+
+        //Find Items in the cache and return them
+        List<Integer> results = itemToPriorityMap.entrySet().stream()
+                .filter(e -> e.getValue() >= 5).map(e -> e.getKey()).collect(Collectors.toList());
+
+        return !results.isEmpty() ? results : List.of(-1);
+    }
+}
---
+++ src/test/java/com/abuob/challenges/advisor/Solution2Test.java
@@ -0,0 +1,96 @@
+package com.abuob.challenges.advisor;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Solution2Test {
+
+    @Test
+    public void oneSecondInCache_noElements() {
+
+        List<Integer> A = List.of(1, 1);
+
+        List<List<Integer>> callLogs = List.of(A);
+        List<Integer> result = Solution2.cacheContents(callLogs);
+
+        assertThat(result).isNotNull()
+                .hasSize(1)
+                .containsExactlyElementsOf(List.of(-1));
+    }
+
+    @Test
+    public void oneSecondInCache_oneElement() {
+        List<Integer> A = List.of(1, 1);
+        List<Integer> B = List.of(1, 1);
+        List<Integer> C = List.of(1, 1);
+        List<Integer> D = List.of(1, 1);
+        List<Integer> E = List.of(1, 1);
+
+        List<List<Integer>> callLogs = List.of(A, B, C, D, E);
+        List<Integer> result = Solution2.cacheContents(callLogs);
+
+        assertThat(result).isNotNull()
+                .hasSize(1)
+                .containsExactlyElementsOf(List.of(1));
+    }
+
+    @Test
+    public void twoSecondInCache_zeroElement() {
+        List<Integer> A = List.of(1, 1);
+        List<Integer> B = List.of(1, 1);
+        List<Integer> C = List.of(1, 1);
+        List<Integer> D = List.of(1, 1);
+        List<Integer> E = List.of(1, 1);
+        List<Integer> F = List.of(2, 2);
+        List<Integer> G = List.of(2, 2);
+
+
+        List<List<Integer>> callLogs = List.of(A, B, C, D, E, F, G);
+        List<Integer> result = Solution2.cacheContents(callLogs);
+
+        assertThat(result).isNotNull()
+                .hasSize(1)
+                .containsExactlyElementsOf(List.of(-1));
+    }
+
+    @Test
+    public void twoSecondInCache_oneElement() {
+        List<Integer> A = List.of(1, 1);
+        List<Integer> B = List.of(1, 1);
+        List<Integer> C = List.of(1, 1);
+        List<Integer> D = List.of(1, 1);
+        List<Integer> E = List.of(1, 1);
+        List<Integer> F = List.of(1, 1);
+        List<Integer> G = List.of(2, 2);
+        List<Integer> H = List.of(2, 2);
+
+        List<List<Integer>> callLogs = List.of(A, B, C, D, E, F, G, H);
+        List<Integer> result = Solution2.cacheContents(callLogs);
+
+        assertThat(result).isNotNull()
+                .hasSize(1)
+                .containsExactlyElementsOf(List.of(1));
+    }
+
+    @Test
+    public void threeSecondInCache_inAndOut() {
+        List<Integer> A = List.of(1, 1);
+        List<Integer> B = List.of(1, 1);
+        List<Integer> C = List.of(1, 1);
+        List<Integer> D = List.of(1, 1);
+        List<Integer> E = List.of(1, 1);
+        List<Integer> F = List.of(2, 2);
+        List<Integer> G = List.of(2, 2);
+        List<Integer> H = List.of(3, 1);
+
+        List<List<Integer>> callLogs = List.of(A, B, C, D, E, F, G, H);
+        List<Integer> result = Solution2.cacheContents(callLogs);
+
+        assertThat(result).isNotNull()
+                .hasSize(1)
+                .containsExactlyElementsOf(List.of(1));
+    }
+}
