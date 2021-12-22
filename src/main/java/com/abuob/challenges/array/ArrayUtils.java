da40ccc6c8533bb9e1e3c562fc7952c8b45e8f8d
Alex Buob <alexander.buob@gmail.com>
2021-12-22 15:29:06 -0600

    add

--- src/main/java/com/abuob/challenges/array/ArrayUtils.java
+++ src/main/java/com/abuob/challenges/array/ArrayUtils.java
@@ -1,6 +1,7 @@
 package com.abuob.challenges.array;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public final class ArrayUtils {
 
@@ -58,5 +59,49 @@ public final class ArrayUtils {
 
     private static long sum(int[] array) {
         return Arrays.stream(array).mapToLong(Long::valueOf).sum();
+    }
+
+
+    public static boolean areArraysEqual(int[] array1, int[] array2) {
+
+        int size1 = array1.length;
+        int size2 = array2.length;
+
+        Map<Integer, Integer> map1 = new HashMap<>();
+        Map<Integer, Integer> map2 = new HashMap<>();
+
+        if (size1 != size2) {
+            return false;
+        }
+
+        for (int i : array1) {
+            Integer count = map1.get(i);
+            if (count == null) {
+                map1.put(i, 1);
+            } else {
+                map1.put(i, ++count);
+            }
+        }
+
+        for (int j : array2) {
+            Integer count = map2.get(j);
+            if (count == null) {
+                map2.put(j, 1);
+            } else {
+                map2.put(j, ++count);
+            }
+        }
+
+        System.out.println("map1:" + mapToString(map1));
+        System.out.println("map2:" + mapToString(map2));
+
+        return map1.equals(map2);
+    }
+
+    private static String mapToString(Map<Integer, ?> map) {
+        String mapAsString = map.keySet().stream()
+                .map(key -> key + "=" + map.get(key))
+                .collect(Collectors.joining(", ", "{", "}"));
+        return mapAsString;
     }
 }
--- src/test/java/com/abuob/challenges/array/ArrayUtilsTest.java
+++ src/test/java/com/abuob/challenges/array/ArrayUtilsTest.java
@@ -19,4 +19,33 @@ public class ArrayUtilsTest {
         assertThat(ArrayUtils.getEquilibriumIndices(new int[]{-1, 3, -4, 5, 1, -6, 2, 1}))
                 .isEqualTo(Lists.newArrayList(1, 3, 7));
     }
+
+    @Test
+    public void areArraysEqual_differentSize() {
+
+        int[] intArray1 = new int[]{1, 2, 3};
+        int[] intArray2 = new int[]{1, 2, 3, 4};
+        assertThat(ArrayUtils.areArraysEqual(intArray1, intArray2)).isFalse();
+
+    }
+
+    @Test
+    public void areArraysEqual_sameSize_equal() {
+
+        int[] intArray1 = new int[]{1, 2, 3};
+        int[] intArray2 = new int[]{3, 2, 1};
+        assertThat(ArrayUtils.areArraysEqual(intArray1, intArray2)).isTrue();
+
+    }
+
+    @Test
+    public void areArraysEqual_sameSize_notEqual() {
+
+        int[] intArray1 = new int[]{1, 2, 3};
+        int[] intArray2 = new int[]{1, 2, 4};
+        assertThat(ArrayUtils.areArraysEqual(intArray1, intArray2)).isFalse();
+
+    }
+
+
 }
